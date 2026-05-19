41a0224fb9ef2ba27a59f036ebdbdae4de99df77
h-nahhas <h.nahhas@eduly.com>
2026-05-19 21:59:13 +0300

    basic controller

---
+++ src/main/java/org/example/devops4/controller/WebController.java
@@ -0,0 +1,19 @@
+package org.example.devops4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/test/java/org/example/devops4/Devops4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devops4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
